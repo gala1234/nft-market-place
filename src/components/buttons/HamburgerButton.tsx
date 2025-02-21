acba05a15feb06e15d86889c842f10ae58c3fce0
Gala <calero.vg@gmail.com>
2025-02-21 14:43:08 +0100

    center hambbutton

--- src/components/buttons/HamburgerButton.tsx
+++ src/components/buttons/HamburgerButton.tsx
@@ -17,17 +17,17 @@ export const HamburgerButton: React.FC<HamburgerButtonProps> = ({
     >
       <div className="w-6 h-6 relative">
         <span
-          className={`absolute h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
+          className={`absolute left-0 h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
             isOpen ? "rotate-45 translate-y-2.5" : "translate-y-0.5"
           }`}
         />
         <span
-          className={`absolute h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
+          className={`absolute left-0 h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
             isOpen ? "opacity-0" : "translate-y-2.5"
           }`}
         />
         <span
-          className={`absolute h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
+          className={`absolute left-0 h-0.5 w-6 bg-current transform transition-all duration-300 ease-in-out ${
             isOpen ? "-rotate-45 translate-y-2.5" : "translate-y-4.5"
           }`}
         />
